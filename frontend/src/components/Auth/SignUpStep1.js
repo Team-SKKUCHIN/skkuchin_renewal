48a830765ab6b69a0f18d8c66bab48970706c795
syj0396 <syj0396@naver.com>
2023-11-25 15:59:57 +0900

    refactor(회원가입): 이전 버튼 클릭 시 data 유지

--- frontend/src/components/Auth/SignUpStep1.js
+++ frontend/src/components/Auth/SignUpStep1.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {  TextField, Button, Typography, Box, Container, Grid, OutlinedInput, InputAdornment, IconButton } from '@mui/material';
 import back from '../../image/arrow_back_ios.png';
 import Image from 'next/image';
@@ -70,11 +70,9 @@ const SignUpStep1 = (props) => {
         setContainNum(/\d/.test(password));
         setContainSpe(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(password));
         setValidLen(password.length >= 8 && password.length <= 16);
-      }
-    
-    const handlePasswordChange = (e) => {
-        const password = e.target.value;
-        props.setData({...props.data, password});
+    }
+
+    const validatePw = (password) => {
         checkPassword(password);
 
         let num = password.search(/[0-9]/g)
@@ -88,28 +86,24 @@ const SignUpStep1 = (props) => {
         } else {
             setValidPW(true);
         }
-        // if ((password.length >= 8 && password.length <= 16) && num >= 0 && eng >= 0) {
-        //     setValidPW(true)
-        //     setValidLen(true)
-        //     setContainNum(true)
-        //     setContaingEng(true)
-        //     //setContainSpe(true)
-        // } else {
-        //     setValidPW(false)
-        //     if (password.length < 8 || password.length > 16) setValidLen(false)
-        //     else setValidLen(true)
-        //     if (num < 0) setContainNum(false) 
-        //     else setContainNum(true)
-        //     if (eng < 0) setContaingEng(false)
-        //     else setContaingEng(true)
-        //     // if (spe < 0) setContainSpe(false)
-        //     // else setContainSpe(true)
-        // }
+    }
+    
+    const handlePasswordChange = (e) => {
+        const password = e.target.value;
+        props.setData({...props.data, password});
+        validatePw(password);
     }
 
     const backClick = () => {
         router.push('/login');
     }
+
+    useEffect(() => {
+        if (props.data.username != '') {
+            checkUsername();
+            validatePw(props.data.password);
+        }
+    }, [])
 
     return (
         <div>
@@ -165,22 +159,6 @@ const SignUpStep1 = (props) => {
                     fontSize: '16px',
                   }}
                 />
-                {/* <input
-                    variant="standard"
-                    placeholder="아이디"
-                    value={props.data.username}
-                    onChange={handleUsernameChange}
-                    style={{
-                        fontSize: '16px',
-                        padding: '20px 15px 21px 12px',
-                        height: '56px',
-                        border: validUsername == null ? '1px solid #E2E2E2' : (validUsername ? '1px solid #12A054' : '1px solid #FF3B3B'),
-                        margin: '8px 0 0px 0',
-                        borderRadius: '8px',
-                        width: '100%',
-                        outline: 'none'
-                    }}
-                /> */}
                 {/* 중복확인 메소드 추가 */}
                 <div style={{display:'flex'}}>
                     {/* <Button variant="contained" onClick={checkUsername} style={{backgroundColor: '#FFCE00', color: '#fff', borderRadius: '8px', width: '47px', height: '20px', fontSize: '9px', padding: '3px 4px', marginTop: '4px', boxShadow: 'none'}}>중복확인</Button> */}
@@ -270,37 +248,6 @@ const SignUpStep1 = (props) => {
                     </div>
                 </div> 
                 }
-                {/* <input
-                    variant="standard"
-                    type="password"
-                    placeholder="비밀번호"
-                    value={props.data.password}
-                    onChange={handlePasswordChange}
-                    style={{
-                        fontSize: '16px',
-                        padding: '20px 15px 21px 12px',
-                        height: '56px',
-                        border: '1px solid #E2E2E2',
-                        margin: '8px 0 4px 0',
-                        borderRadius: '8px',
-                        width: '100%',
-                        outline: 'none'
-                    }}
-                /> */}
-                {/* <Grid container>
-                    <Grid item>
-                        <Typography style={{fontSize: '12px', color: containEng ? '#12A054' : '#777777', marginRight: '8px'}}>&#10003;영문</Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography style={{fontSize: '12px', color: containNum ? '#12A054' : '#777777', marginRight: '8px'}}>&#10003;숫자</Typography>
-                    </Grid>
-                    {/* <Grid item>
-                        <Typography style={{fontSize: '12px', color: containSpe ? '#12A054' : '#777777', marginRight: '8px'}}>&#10003;특수문자</Typography>
-                    </Grid> 
-                    <Grid item>
-                        <Typography style={{fontSize: '12px', color: validLen ? '#12A054' : '#777777', marginRight: '8px'}}>&#10003;8~16자 이내</Typography>
-                    </Grid>
-                </Grid> */}
 
                 <Typography style={{fontSize: '12px', fontWeight: '900', marginTop: '16px', marginLeft: '4px', color: '#3C3C3C'}}>비밀번호 확인</Typography>
                 <OutlinedInput
@@ -309,12 +256,6 @@ const SignUpStep1 = (props) => {
                 value={props.data.re_password}
                 placeholder="비밀번호 재입력"
                 onChange={(e) => {
-                    // const newPassword = e.target.value;
-                    // if (newPassword === password) {
-                    //   setIsCorrect('none');
-                    // } else {
-                    //   setIsCorrect('wrong');
-                    // }
                     props.setData({...props.data, re_password: e.target.value})
                 }}
                 style={{width:'100%', marginTop: '8px'}}
@@ -346,23 +287,6 @@ const SignUpStep1 = (props) => {
                     fontSize: '16px',
                   }}
                 />
-                {/* <input
-                    variant="standard"
-                    type="password"
-                    placeholder="비밀번호 재입력"
-                    value={props.data.re_password}
-                    onChange={(e) => props.setData({...props.data, re_password: e.target.value})}
-                    style={{
-                        fontSize: '16px',
-                        padding: '20px 15px 21px 12px',
-                        height: '56px',
-                        border: (props.data.re_password != '') && props.data.password !== props.data.re_password ? '1px solid #FF3B3B' : '1px solid #E2E2E2',
-                        margin: '8px 0 4px 0',
-                        borderRadius: '8px',
-                        width: '100%',
-                        outline: 'none'
-                    }}
-                /> */}
                 {(props.data.re_password != '') && props.data.password !== props.data.re_password ? <Typography sx={{fontSize: '12px', fontWeight: '500', padding: '5px 0 0 5px', color: '#FF3B3B'}}>비밀번호가 일치하지 않습니다.</Typography>
                     : <div style={{height: '18px'}}></div>}
             </div>
