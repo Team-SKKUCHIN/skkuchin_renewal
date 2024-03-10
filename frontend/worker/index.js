11be2ec67043ec75c179225b4920a4dc86b2997b
myungjunlee <dlaudwns789@gmail.com>
2024-03-11 05:22:06 +0900

    remove service worker cache

--- frontend/worker/index.js
+++ frontend/worker/index.js
@@ -46,14 +46,14 @@ self.addEventListener('notificationclick', function (event) {
     )
 })
 
-self.addEventListener('message', async event => {
-    if (event.data && event.data.action === 'CACHE_NEW_ROUTE') {
-        caches.open('others').then(cache =>
-            cache.match(event.source.url).then(res => {
-                if (res === undefined) {
-                    return cache.add(event.source.url)
-                }
-            })
-        )
-    }
-})
+// self.addEventListener('message', async event => {
+//     if (event.data && event.data.action === 'CACHE_NEW_ROUTE') {
+//         caches.open('others').then(cache =>
+//             cache.match(event.source.url).then(res => {
+//                 if (res === undefined) {
+//                     return cache.add(event.source.url)
+//                 }
+//             })
+//         )
+//     }
+// })
